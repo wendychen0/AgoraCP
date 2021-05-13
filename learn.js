fb9e6a8a9c1dd966c0698293838e29589707af92
Wendy Chen <wendy.chen5272@gmail.com>
2021-05-12 20:51:39 -0700

    week 2 validation

---
+++ learn.js
@@ -0,0 +1,33 @@
+let name = "Wendy"
+let age = 18
+let isSingle = false
+
+name = "w"
+
+const interestRate = 0.3
+let person = {
+    name: "Wendy",
+    age: 18,
+    isSingle: true,
+}
+
+let choice = 'name'
+
+console.log(person[choice])
+
+choice = 'age'
+
+if (age >= 21){
+    console.log("is of legal drinking age")
+}
+
+function welcome(name){
+    console.log('hi'+name)
+}
+
+function adder(n1, n2){
+    return n1 + n2
+}
+let sum = adder(2,3)
+
+console.log(sum)
